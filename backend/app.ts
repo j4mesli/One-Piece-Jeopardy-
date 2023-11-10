835f7321b82ee1397696a453e57c51f94505d142
James Li <jamesli196q@gmail.com>
2023-11-09 21:24:09 -0500

    changed MVC layout (heroku doesn't like it)

--- backend/app.ts
+++ backend/app.ts
@@ -5,6 +5,8 @@ import path from 'path';
 import setup from "./setup";
 import userRoutes from "./routes/userRoutes";
 import gameRoutes from "./routes/gameRoutes";
+import { loginHandler, logoutHandler, registerHandler, verifySessionHandler } from './controllers/userController';
+import { fetchTestsHandler, evaluateTestHandler, deleteTestHandler, fetchQuestionHandler, evaluateQuestionHandler } from './controllers/gameController';
 
 const app = express();
 
@@ -24,7 +26,15 @@ app.use(cors());
 setup();
 
 // route handling
-app.use("/", userRoutes);
-app.use("/", gameRoutes);
+app.post('/logout', logoutHandler);
+app.post('/login', loginHandler);
+app.post('/register', registerHandler);
+app.get('/verifySession', verifySessionHandler);
+
+app.get('/fetchTestQuestions', fetchTestsHandler);
+app.post('/evaluateTest', evaluateTestHandler);
+app.delete('/deleteTest', deleteTestHandler);
+app.get('/fetchQuestion', fetchQuestionHandler);
+app.post('/evaluateQuestion', evaluateQuestionHandler);
 
 app.listen(process.env.PORT || 3000, () => { console.log("backend listening on port 3000 at http://localhost:3000") });
